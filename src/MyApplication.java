0cf8a5da2486f36c84a359222fe678562f686efa
Bas van Schaik <github@s.traiectum.net>
2020-09-11 00:04:57 +0100

    hello world

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,26 +1,7 @@
 package src;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
 class MyApplication {    
     public static void main(final String[] argv) throws Exception {
-        ZipInputStream zipstream = new ZipInputStream(
-            new FileInputStream(new File("my-zip-file.zip")));
-        
-        
-        ZipEntry entry;
-        while ((entry = zipstream.getNextEntry()) != null){
-            // Get the name of the file inside the zip archive
-            String filename = entry.getName();
-
-            // My manager wants all filenames to be lowercase
-            String loweredFilename = filename.toLowerCase();
-
-            // TODO: something with a FileOutputStream and a buffer?            
-        }
+        System.out.println("Hello world!");
     }
 }
